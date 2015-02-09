d064ba5b846b16e4cb923d5c773df836a8fbdafa
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-02-09 20:59:22 +0200

    Add basic structure of the project

---
+++ ConsoleGame/ConsoleGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    class ConsoleGame
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
