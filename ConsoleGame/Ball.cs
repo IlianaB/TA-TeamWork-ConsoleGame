12706f16cd02f678fd17064df58dcdd49a75ca88
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-02-13 16:31:23 +0200

    Add class for the ball and move it

---
+++ ConsoleGame/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleGame
+{
+    class Ball
+    {
+        private int x;
+        private int y;
+        private int oldX;
+        private int oldY;
+        private bool rightDirection;
+        private bool topDirection;
+        private ConsoleColor color;
+
+        public Ball(int x, int y, ConsoleColor color)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            this.rightDirection = true;
+            this.topDirection = true;
+        }
+
+        public void ChangePosition()
+        {
+            this.oldX = this.x;
+            this.oldY = this.y;
+
+            this.x = this.rightDirection ? this.x + 1 : this.x - 1;
+            this.y = this.topDirection ? this.y - 1 : this.y + 1;
+        }
+
+        public void Draw(char symbol)
+        {
+            Console.ForegroundColor = this.color;
+
+            // remove old ball
+            Console.SetCursorPosition(this.oldX, this.oldY);
+            Console.Write(' ');
+
+            // write new ball
+            Console.SetCursorPosition(this.x, this.y);
+            Console.Write(symbol);
+        }
+    }
+}
--- ConsoleGame/ConsoleGame.cs
+++ ConsoleGame/ConsoleGame.cs
@@ -8,9 +8,19 @@ namespace ConsoleGame
         public const int windowWidth = 50;
         public const int windowHeight = 20;
 
+        private static Ball ball = new Ball(windowWidth / 2 + 1, windowHeight - 2, ConsoleColor.Red);
+
         static void Main()
         {
             SetupGameField();
+
+            while (true)
+            {
+                ball.ChangePosition();
+                ball.Draw('*');
+
+                Thread.Sleep(100);
+            }
         }
 
         private static void SetupGameField()
