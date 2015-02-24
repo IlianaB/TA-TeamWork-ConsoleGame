f0ddb19ebb2549af952f637db007e5e0aa920764
pzubev <p-zubev@operamail.com>
2015-02-24 17:12:13 +0200

    Bug fixed when ball hits TopWall

--- ConsoleGame/Ball.cs
+++ ConsoleGame/Ball.cs
@@ -219,7 +219,7 @@ namespace ConsoleGame
 
         public bool IsTopWallCollision()
         {
-            return this.y == +1;
+            return this.y == +2;
         }
 
         public void Draw(char symbol)
